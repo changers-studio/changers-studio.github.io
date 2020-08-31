88b3eeae5177c01ca8096b01bf69f8dd92c81b52
Nikita <53001925+Bus1n@users.noreply.github.com>
2020-08-31 18:13:53 +0300

    yut

--- js/custom.js
+++ js/custom.js
@@ -208,9 +208,7 @@ var $win = $(window),
         }
 
 var scrollHeight = Math.max(
-  document.body.scrollHeight, document.documentElement.scrollHeight,
-  document.body.offsetHeight, document.documentElement.offsetHeight,
-  document.body.clientHeight, document.documentElement.clientHeight
+  document.body.scrollHeight
 )
 
 
