0d406e623d55672b777bfdb17ac6c89bc77b1ba4
Nikita <53001925+Bus1n@users.noreply.github.com>
2021-02-06 00:10:36 +0200

    Utopia Mining 1.0

--- js/preloader.js
+++ js/preloader.js
@@ -1,11 +1,13 @@
-$(document).ready(function(){
-	
-	$('body').addClass('overflow-y');
-	
-	$('.preloader').delay(2000).fadeOut(1000);
+$(document).ready(function () {
+  $(window).on('load', function () {
+    AOS.refresh();
+  });
 
-	setTimeout(function() {
+  $('body').addClass('overflow-y');
+
+  $('.preloader').delay(2500).fadeOut(1000);
+
+  setTimeout(function () {
     $('body').removeClass('overflow-y');
-  }, 2000);
-
+  }, 2500);
 });
