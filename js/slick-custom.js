0d605b8af0d833187669e55cdcc0d8048abc1a55
Nikita <53001925+Bus1n@users.noreply.github.com>
2021-05-03 16:40:52 +0200

    Wunity 2.0

---
+++ js/slick-custom.js
@@ -0,0 +1,18 @@
+jQuery(function () {
+	if ($(window).width() < 992) {
+		$('.captions_flex').slick({
+			infinite: false,
+			slidesToShow: 2,
+			slidesToScroll: 2,
+			prevArrow: $('.caption-prev'),
+			nextArrow: $('.caption-next')
+		})
+	}
+
+	$('.captions_flex').on('afterChange', function (event, slick) {
+		$('.captions__counter .counter__current').html(
+			(slick.slickCurrentSlide() + 2) / 2
+		)
+		$('.captions__counter .counter__all').html(slick.slideCount / 2)
+	})
+})
