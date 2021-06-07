3df6c2be0f5516d265b71cf6df81826f0c46f47b
Bus1n <nikita.tytskyi@gmail.com>
2021-06-07 16:57:41 +0300

    Allo 2.1

--- js/custom.js
+++ js/custom.js
@@ -13,6 +13,24 @@ jQuery(function () {
 	})
 
 	$('.magnific-popup').magnificPopup()
+
+	//E-mail Ajax Send
+	$('form').on('submit', function () {
+		//Change
+		var th = $(this)
+		$.ajax({
+			type: 'POST',
+			url: 'mail.php', //Change
+			data: th.serialize(),
+		}).done(function () {
+			alert('Thank you!')
+			setTimeout(function () {
+				// Done Functions
+				th.trigger('reset')
+			}, 1000)
+		})
+		return false
+	})
 })
 
 $('.header__hamburger').on('click', function () {
