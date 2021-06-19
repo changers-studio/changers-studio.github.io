5d62ab58a3913726beec6a2091bb67ae36090975
Bus1n <nikita.tytskyi@gmail.com>
2021-06-19 03:34:49 +0300

    Ассоциация 1.0

--- js/custom.js
+++ js/custom.js
@@ -1,44 +1,53 @@
 jQuery(function () {
-	$('.reviews__slider').slick({
+	// code
+})
+
+$('.dropdown__heading').on('click', function () {
+	$(this).toggleClass('_active').next().slideToggle()
+	$('.dropdown__heading').not(this).removeClass('_active').next().slideUp()
+})
+
+let body = $('body')
+let hamburger = $('.header .hamburger')
+let sidebar = $('.header .sidebar')
+let plug = $('.header .sidebar__plug')
+let toggleSidebar = $(
+	'.header .hamburger, .header .sidebar__plug, .header .sidebar__close'
+)
+
+toggleSidebar.on('click', function () {
+	hamburger.toggleClass('_active')
+	sidebar.toggleClass('_visible')
+	plug.toggleClass('_visible')
+	body.toggleClass('scroll_disable')
+})
+
+if ($('.partners__slider').length > 0) {
+	$('.partners__slider').slick({
 		infinite: true,
-		slidesToShow: 3,
+		slidesToShow: 4,
 		SlidesToScroll: 1,
-		speed: 800,
+		speed: 1000,
 		touchMove: false,
 		responsive: [
 			{
-				breakpoint: 1240,
+				breakpoint: 992,
 				settings: {
 					slidesToShow: 2,
 				},
 			},
-			{
-				breakpoint: 992,
-				settings: {
-					slidesToShow: 1,
-				},
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					centerMode: true,
-					variableWidth: true,
-					arrows: false,
-				},
-			},
 		],
 	})
-})
+}
 
-var body = $('body')
-var sidebar = $('.header__sidebar')
-var plug = $('.header__sidebar-plug')
-var toggleSidebar = $(
-	'.header__hamburger, .header__sidebar-plug, .header__sidebar-close'
-)
-
-toggleSidebar.on('click', function () {
-	sidebar.toggleClass('_visible')
-	plug.toggleClass('_visible')
-	body.toggleClass('scroll_disable')
+$('ul.tabs__caption').on('click', 'li:not(._active)', function () {
+	$(this)
+		.addClass('_active')
+		.siblings()
+		.removeClass('_active')
+		.closest('div.tabs')
+		.find('div.tabs__content')
+		.removeClass('_active')
+		.eq($(this).index())
+		.addClass('_active')
 })
