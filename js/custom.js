737b8f063b14671a6e0c82f30ad06bedd9243009
Bus1n <nikita.tytskyi@gmail.com>
2021-07-25 22:49:53 +0300

    Garant 1.0

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,11 @@
 jQuery(function () {
-	// code
+	//
 })
 
-// Sidebar
-let sidebar = $('.sidebar')
-let toggleSidebar = $('.header .hamburger, .sidebar__close')
-
-toggleSidebar.on('click', function () {
-	sidebar.toggleClass('visible')
-})
-//
-
-// Dropdown
-$('.dropdown__heading').on('click', function () {
-	$(this).toggleClass('_active').next().slideToggle()
-	$('.dropdown__heading').not(this).removeClass('_active').next().slideUp()
+$('.header__menu').on('click', function () {
+	$('.sidebar').fadeIn()
 })
 
-// Button to top
-$('body').on('click', '.link_move', function (event) {
-	event.preventDefault()
-	var id = $(this).attr('href'),
-		top = $(id).offset().top
-	$('body,html').animate({ scrollTop: top - 50 }, 1500)
+$('.sidebar__close').on('click', function () {
+	$('.sidebar').fadeOut()
 })
