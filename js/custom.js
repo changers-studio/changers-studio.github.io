f32b1a0c5199318a10966acedfd6b0b6a45c8086
Nikita <53001925+Bus1n@users.noreply.github.com>
2021-09-09 04:18:23 +0400

    Hiroster

--- js/custom.js
+++ js/custom.js
@@ -1,272 +1,104 @@
 jQuery(function () {
+	if ($('*').is('.advisors__slider')) {
+		$('.advisors__slider').slick({
+			touchMove: false,
+			speed: 800,
+			dots: true,
+			slidesToShow: 4,
+			responsive: [
+				{
+					breakpoint: 1200,
+					settings: {
+						slidesToShow: 3,
+					},
+				},
+				{
+					breakpoint: 992,
+					settings: {
+						slidesToShow: 2,
+					},
+				},
+				{
+					breakpoint: 768,
+					settings: {
+						slidesToShow: 1,
+						fade: true,
+					},
+				},
+			],
+		})
+	}
+
 	// Select2
-	if ($('*').is('.select2')) {
-		$('.select2').select2({
+	if ($('*').is('.filter__item-select')) {
+		$('.filter__item-select').select2({
 			minimumResultsForSearch: -1,
 			width: '100%',
-			selectionCssClass: 'select__heading',
-			dropdownCssClass: 'select__dropdown',
-		})
-	}
-
-	if ($('*').is('.favorites-select')) {
-		$('.favorites-select').select2({
-			minimumResultsForSearch: -1,
-			width: 'auto',
-			selectionCssClass: 'favorites-select__heading',
-			dropdownCssClass: 'favorites-select__dropdown',
-		})
-	}
-
-	if ($('*').is('.sort-select')) {
-		$('.sort-select').select2({
-			minimumResultsForSearch: -1,
-			width: 'auto',
-			selectionCssClass: 'sort-select__heading',
-			dropdownCssClass: 'sort-select__dropdown',
-		})
-	}
-
-	if ($('*').is('.size-select')) {
-		$('.size-select').select2({
-			minimumResultsForSearch: -1,
-			width: '100%',
-			selectionCssClass: 'size-select__heading',
-			dropdownCssClass: 'size-select__dropdown',
+			selectionCssClass: 'filter-select__heading',
+			dropdownCssClass: 'filter-select__dropdown',
 		})
 	}
 
 	$('b[role="presentation"]').hide()
 	$('.select2-selection__arrow').append('<i class="fas fa-chevron-down"></i>')
 
-	// Dropdowns
-	$(
-		'.lang__heading, .filter__item-heading, .filter__heading_mobile, .catalog__sort-heading, .product__dropdown-heading'
-	).on('click', function () {
-		$(this).toggleClass('active').next().slideToggle(300)
-	})
-
-	$('.tel .dropdown__heading i').on('click', function () {
-		$(this).parent().toggleClass('active').next().slideToggle()
-	})
-
-	// Input tel mask
-	if ($('*').is('input[type="tel"]')) {
-		$('input[type=tel]').inputmask({
-			mask: '+99 (999) 999 99 99',
-			showMaskOnHover: false,
+	// Dropdown
+	if ($('*').is('.player__dropdown-heading')) {
+		$('.player__dropdown-heading').on('click', function () {
+			$(this)
+				.toggleClass('active')
+				.closest('.player')
+				.find('.player__dropdown-body')
+				.slideToggle()
 		})
 	}
 
-	// Reset form
-	$('.form__reset').on('click', function () {
-		$(this).closest('form').trigger('reset')
-	})
-
-	// Magnific popup
-	$('.magnific-popup').magnificPopup({
-		removalDelay: 350,
-		mainClass: 'mfp-fade',
-		fixedContentPos: false,
-		callbacks: {
-			open: function () {
-				$('body').addClass('scroll_disable')
-			},
-			close: function () {
-				$('body').removeClass('scroll_disable')
-			},
-		},
-	})
-
-	// Slick sliders
-	if ($('*').is('.main__slider')) {
-		$('.main__slider').slick({
-			fade: true,
-			arrows: false,
-			dots: true,
-			infinite: true,
-			autoplay: true,
-			speed: 500,
-			autoplaySpeed: 3000,
-			touchMove: false,
-		})
-	}
-
-	if ($('*').is('.product__slider')) {
-		$('.product__slider').slick({
-			dots: true,
-			infinite: true,
-			speed: 800,
-			touchMove: false,
-			responsive: [
-				{
-					breakpoint: 992,
-					settings: {
-						arrows: false,
-					},
-				},
-			],
-		})
-	}
-
-	if ($('*').is('.categories__slider')) {
-		$('.categories__slider').slick({
-			dots: true,
-			infinite: true,
-			speed: 800,
-			touchMove: false,
-			slidesToShow: 4,
-			responsive: [
-				{
-					breakpoint: 992,
-					settings: {
-						arrows: false,
-						slidesToShow: 2,
-					},
-				},
-				{
-					breakpoint: 576,
-					settings: {
-						slidesToShow: 2,
-						variableWidth: true,
-						dots: false,
-						arrows: false,
-					},
-				},
-			],
-		})
-	}
-
-	if ($('*').is('.recently-viewed__slider')) {
-		$('.recently-viewed__slider').slick({
-			dots: true,
-			infinite: true,
-			touchMove: false,
-			slidesToShow: 4,
-			responsive: [
-				{
-					breakpoint: 1520,
-					settings: {
-						slidesToShow: 1,
-						variableWidth: true,
-					},
-				},
-				{
-					breakpoint: 992,
-					settings: {
-						arrows: false,
-						slidesToShow: 1,
-						variableWidth: true,
-					},
-				},
-			],
-		})
-	}
-
-	if ($('*').is('.product__slider-main')) {
-		$('.product__slider-main').slick({
-			arrows: false,
-			fade: true,
-			asNavFor: '.product__slider-nav',
-			responsive: [
-				{
-					breakpoint: 992,
-					settings: {
-						dots: true,
-					},
-				},
-			],
-		})
-
-		$('.product__slider-nav').slick({
-			slidesToShow: 5,
-			asNavFor: '.product__slider-main',
-			focusOnSelect: true,
-			vertical: true,
+	// Filter dropup
+	if ($('*').is('.filter__button-mobile')) {
+		$('.filter__button-mobile, .filter__close').on('click', function () {
+			$('.filter').toggleClass('active')
 		})
 	}
 
 	// Range
-	if ($('*').is('.range')) {
-		let $range = $('.range__slider'),
-			$inputFrom = $('.range__from'),
-			$inputTo = $('.range__to'),
-			instance,
-			min = 100,
-			max = 100000,
-			from = 0,
-			to = 0
-
-		$range.ionRangeSlider({
+	if ($('*').is('.range-input')) {
+		$('.range-input').ionRangeSlider({
 			skin: 'round',
-			type: 'double',
-			min: min,
-			max: max,
-			from: 199,
-			to: 3000,
-			onStart: updateInputs,
-			onChange: updateInputs,
-		})
-		instance = $range.data('ionRangeSlider')
-
-		function updateInputs(data) {
-			from = data.from
-			to = data.to
-
-			$inputFrom.prop('value', from)
-			$inputTo.prop('value', to)
-		}
-
-		$inputFrom.on('input', function () {
-			var val = $(this).prop('value')
-
-			// validate
-			if (val < min) {
-				val = min
-			} else if (val > to) {
-				val = to
-			}
-
-			instance.update({
-				from: val,
-			})
-		})
-
-		$inputTo.on('input', function () {
-			var val = $(this).prop('value')
-
-			// validate
-			if (val < from) {
-				val = from
-			} else if (val > max) {
-				val = max
-			}
-
-			instance.update({
-				to: val,
-			})
+			min: 0,
+			max: 100000,
 		})
 	}
 })
 
-// $(document).on('mouseup', function (e) {
-// 	let lang = $('header__tel')
+$('.dropdown').on('click', function () {
+	$(this).toggleClass('active').next().slideToggle()
+	$('.dropdown').not(this).removeClass('active').next().slideUp()
+})
 
-// 	if (!lang.is(e.target) && lang.has(e.target).length === 0) {
-// 		$('.lang__heading').removeClass('_active')
-// 		$('.lang__dropdown').slideUp()
-// 	}
-// })
-
-// Hamburger
+// Main hamburger
 let body = $('body')
-let hamburger = $('.header__hamburger')
-let dropdownMenu = $('.header._mobile .dropdown')
+let sidebar = $('.header__sidebar')
+let plug = $('.header__sidebar-plug')
+let toggleSidebar = $(
+	'.header__hamburger, .header__sidebar-plug, .header__sidebar-close'
+)
 
-hamburger.on('click', function () {
-	hamburger.toggleClass('active')
-	dropdownMenu.slideToggle()
+toggleSidebar.on('click', function () {
+	sidebar.toggleClass('active')
+	plug.toggleClass('active')
 	body.toggleClass('scroll_disable')
+})
+
+// Dashboard hamburger
+let bodyDashboard = $('.p-dashboard')
+let sidebarDashboard = $('.p-dashboard .sidebar-menu')
+let toggleSidebarDashboard = $(
+	'.dashboard .top-line__hamburger, .sidebar-menu__close'
+)
+
+toggleSidebarDashboard.on('click', function () {
+	sidebarDashboard.toggleClass('active')
+	bodyDashboard.toggleClass('scroll_disable')
 })
 
 // Tabs
@@ -280,119 +112,4 @@ $('ul.tabs__caption').on('click', 'li:not(.active)', function () {
 		.removeClass('active')
 		.eq($(this).index())
 		.addClass('active')
-		.hide()
-		.fadeIn(500)
-
-	$('.categories__slider').slick('refresh')
-	$('.product__slider-nav, .product__slider-main').slick('reinit')
-})
-
-$('ul.categories__age').on('click', 'li:not(.active)', function () {
-	$(this).addClass('active').siblings().removeClass('active')
-})
-
-// Toggle password
-$('.toggle-password').on('click', function () {
-	$(this).toggleClass('_show')
-	var input = $(this).prev()
-	if (input.attr('type') == 'password') {
-		input.attr('type', 'text')
-	} else {
-		input.attr('type', 'password')
-	}
-})
-
-// Delete functions
-$('.favorites__item-delete').on('click', function () {
-	$(this).closest('div.favorites__item').hide()
-})
-
-$('.basket__item-delete').on('click', function () {
-	$(this).closest('div.basket__item').hide()
-})
-
-// Basket items amount
-let amountBasketItems = 1
-
-$('.basket__item-add').on('click', function () {
-	$(this)
-		.closest('.basket__item-amount')
-		.find('.basket__item-number')
-		.attr('value', (amountBasketItems += 1))
-
-	if (amountBasketItems === 1) {
-		$(this)
-			.closest('.basket__item-amount')
-			.find('.basket__item-remove')
-			.addClass('_disable')
-	} else {
-		$(this)
-			.closest('.basket__item-amount')
-			.find('.basket__item-remove')
-			.removeClass('_disable')
-	}
-})
-
-$('.basket__item-remove').on('click', function () {
-	if (amountBasketItems !== 1) {
-		$(this)
-			.closest('.basket__item-amount')
-			.find('.basket__item-number')
-			.attr('value', (amountBasketItems -= 1))
-	}
-
-	if (amountBasketItems === 1) {
-		$(this)
-			.closest('.basket__item-amount')
-			.find('.basket__item-remove')
-			.addClass('_disable')
-	} else {
-		$(this)
-			.closest('.basket__item-amount')
-			.find('.basket__item-remove')
-			.removeClass('_disable')
-	}
-})
-
-// Product items amount
-let amountProductItems = 1
-
-$('.product__amount-add').on('click', function () {
-	$(this)
-		.closest('.product__amount')
-		.find('.product__amount-number')
-		.attr('value', (amountProductItems += 1))
-
-	if (amountProductItems === 1) {
-		$(this)
-			.closest('.product__amount')
-			.find('.product__amount-remove')
-			.addClass('_disable')
-	} else {
-		$(this)
-			.closest('.product__amount')
-			.find('.product__amount-remove')
-			.removeClass('_disable')
-	}
-})
-
-$('.product__amount-remove').on('click', function () {
-	if (amountProductItems !== 1) {
-		$(this)
-			.closest('.product__amount')
-			.find('.product__amount-number')
-			.attr('value', (amountProductItems -= 1))
-	}
-
-	if (amountProductItems === 1) {
-		$(this)
-			.closest('.product__amount')
-			.find('.product__amount-remove')
-			.addClass('_disable')
-	} else {
-		$(this)
-			.closest('.product__amount-amount')
-			.find('.product__amount-remove')
-			.removeClass('_disable')
-	}
 })
