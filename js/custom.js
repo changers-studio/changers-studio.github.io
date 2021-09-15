dc51ff7f8eaba9b39f047e3b9834dc3491103e27
Nikita <53001925+Bus1n@users.noreply.github.com>
2021-09-15 16:54:45 +0400

    Financial 1.0

--- js/custom.js
+++ js/custom.js
@@ -1,115 +1,86 @@
 jQuery(function () {
-	if ($('*').is('.advisors__slider')) {
-		$('.advisors__slider').slick({
-			touchMove: false,
-			speed: 800,
-			dots: true,
-			slidesToShow: 4,
-			responsive: [
-				{
-					breakpoint: 1200,
-					settings: {
-						slidesToShow: 3,
-					},
-				},
-				{
-					breakpoint: 992,
-					settings: {
-						slidesToShow: 2,
-					},
-				},
-				{
-					breakpoint: 768,
-					settings: {
-						slidesToShow: 1,
-						fade: true,
-					},
-				},
-			],
-		})
-	}
+	AOS.init({
+		duration: 1000,
+		once: true,
+	})
 
-	// Select2
-	if ($('*').is('.filter__item-select')) {
-		$('.filter__item-select').select2({
-			minimumResultsForSearch: -1,
-			width: '100%',
-			selectionCssClass: 'filter-select__heading',
-			dropdownCssClass: 'filter-select__dropdown',
-		})
-	}
+	document.addEventListener('load', AOS.refresh())
 
-	$('b[role="presentation"]').hide()
-	$('.select2-selection__arrow').append('<i class="fas fa-chevron-down"></i>')
-
-	// Dropdown
-	if ($('*').is('.player__dropdown-heading')) {
-		$('.player__dropdown-heading').on('click', function () {
-			$(this)
-				.toggleClass('active')
-				.closest('.player')
-				.find('.player__dropdown-body')
-				.slideToggle()
-		})
-	}
-
-	// Filter dropup
-	if ($('*').is('.filter__button-mobile')) {
-		$('.filter__button-mobile, .filter__close').on('click', function () {
-			$('.filter').toggleClass('active')
-		})
-	}
-
-	// Range
-	if ($('*').is('.range-input')) {
-		$('.range-input').ionRangeSlider({
-			skin: 'round',
-			min: 0,
-			max: 100000,
-		})
-	}
+	window.addEventListener('scroll', AOS.refresh())
 })
 
-$('.dropdown').on('click', function () {
-	$(this).toggleClass('active').next().slideToggle()
-	$('.dropdown').not(this).removeClass('active').next().slideUp()
-})
-
-// Main hamburger
+// Hamburger menu
 let body = $('body')
-let sidebar = $('.header__sidebar')
-let plug = $('.header__sidebar-plug')
-let toggleSidebar = $(
-	'.header__hamburger, .header__sidebar-plug, .header__sidebar-close'
-)
+let hamburger = $('.header__hamburger')
+let sidebar = $('.header__menu')
+let plug = $('.header__plug')
+let toggleSidebar = $('.header__hamburger, .header__plug, .header__menu-close')
 
 toggleSidebar.on('click', function () {
-	sidebar.toggleClass('active')
-	plug.toggleClass('active')
+	hamburger.toggleClass('_active')
+	sidebar.toggleClass('_visible')
+	plug.toggleClass('_visible')
 	body.toggleClass('scroll_disable')
 })
 
-// Dashboard hamburger
-let bodyDashboard = $('.p-dashboard')
-let sidebarDashboard = $('.p-dashboard .sidebar-menu')
-let toggleSidebarDashboard = $(
-	'.dashboard .top-line__hamburger, .sidebar-menu__close'
-)
-
-toggleSidebarDashboard.on('click', function () {
-	sidebarDashboard.toggleClass('active')
-	bodyDashboard.toggleClass('scroll_disable')
+// Sliders
+$('.industries__slider').slick({
+	infinite: false,
+	arrows: false,
+	touchMove: false,
+	variableWidth: true,
+	speed: 500,
+	autoplay: true,
+	autoplaySpeed: 5000,
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				variableWidth: false,
+			},
+		},
+	],
 })
 
-// Tabs
-$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
-	$(this)
-		.addClass('active')
-		.siblings()
-		.removeClass('active')
-		.closest('div.tabs')
-		.find('div.tabs__content')
-		.removeClass('active')
-		.eq($(this).index())
-		.addClass('active')
+$('.slider__container').slick({
+	infinite: false,
+	touchMove: false,
+	speed: 500,
+	slidesToShow: 3,
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2,
+			},
+		},
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+			},
+		},
+	],
+})
+
+$('.team-slider').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	dots: false,
+	infinite: true,
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 1,
+			},
+		},
+	],
+})
+
+$('body').on('click', '.link_move', function (event) {
+	event.preventDefault()
+	var id = $(this).attr('href'),
+		top = $(id).offset().top
+	$('body,html').animate({ scrollTop: top }, 1500)
 })
