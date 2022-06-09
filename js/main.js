88163dfb700f5a93173d650b61fd23196ed1b5f9
Nikita <53001925+Bus1n@users.noreply.github.com>
2022-06-09 19:51:16 +0400

    Emplab

--- js/main.js
+++ js/main.js
@@ -1,17 +1,364 @@
+const body = $('body')
+const hamburger = $('.header__hamburger')
+const sidebar = $('.header__sidebar')
+const sidebarPlug = $('.header__sidebar-plug')
+const toggleSidebar = $(
+	'.header__hamburger, .header__sidebar-plug, .header__sidebar-close'
+)
+
+const header = $('.header')
+const headerHeight = $('.header').height()
+const headerWidth = $('.header').width()
+const headerDropdown = $('.header__dropdown')
+const headerDropdownPlug = $('.header__dropdown-plug')
+const headerDropdownBtn = $('.header__dropdown-btn')
+
+const reviewsSlider = $('.reviews__slider')
+const reviewsSliderMobile = $('.reviews__slider-mobile')
+
+const postsSlider = $('.posts__slider')
+
 document.addEventListener('DOMContentLoaded', function () {
-	// Sync height image & sidebar
-	$(window).on('resize load', function () {
-		const sidebarHeight = $('.sidebar').outerHeight()
-		$('.main__image').height(sidebarHeight)
+	// Preloader
+	let time
+	if (body.hasClass('ishome')) {
+		$('body').addClass('scroll_disabled')
+		time = 1500
+	} else {
+		time = 0
+	}
+
+	let PercentageID = $('#percentage'),
+		start = 0,
+		end = 100,
+		durataion = time
+	animateValue(PercentageID, start, end, durataion)
+
+	function animateValue(id, start, end, duration) {
+		var range = end - start,
+			current = start,
+			increment = end > start ? 1 : -1,
+			stepTime = Math.abs(Math.floor(duration / range)),
+			obj = $(id)
+
+		var timer = setInterval(function () {
+			current += increment
+			$(obj).text(current + '%')
+
+			if (current == end) {
+				clearInterval(timer)
+			}
+		}, stepTime)
+	}
+
+	setTimeout(() => {
+		$('body').addClass('page-loaded').removeClass('scroll_disabled')
+	}, time)
+	// eof
+
+	// Page transition
+	$('.transition-link').on('click', function (e) {
+		var target = $(this).attr('target')
+		var fancybox = $(this).data('fancybox')
+		var url = this.getAttribute('href')
+		if (
+			target != '_blank' &&
+			typeof fancybox == 'undefined' &&
+			url.indexOf('#') < 0
+		) {
+			e.preventDefault()
+			var url = this.getAttribute('href')
+			if (url.indexOf('#') != -1) {
+				var hash = url.substring(url.indexOf('#'))
+
+				if ($('body ' + hash).length != 0) {
+					$('.page-transition').removeClass('active')
+				}
+			} else {
+				$('.page-transition').toggleClass('active')
+				setTimeout(function () {
+					window.location = url
+				}, 1300)
+				setTimeout(() => {
+					$('.page-transition').removeClass('active')
+				}, 1800)
+			}
+		}
+	})
+	// eof
+
+	// Magnific-popup
+	$('.mfp').magnificPopup({
+		removalDelay: 300,
+		mainClass: 'mfp-fade',
+		fixedContentPos: false,
+		callbacks: {
+			open: function () {
+				body.css('overflow-y', 'hidden')
+			},
+			close: function () {
+				body.css('overflow-y', 'auto')
+			},
+		},
+	})
+	// eof
+
+	// AOS
+	AOS.init({
+		duration: 800,
+		once: true,
 	})
 	// eof
 })
 
-// Dropdown
-$('.dropdown-toggle').on('click', function () {
-	$(this).toggleClass('active').next().slideToggle(200)
-	$('.dropdown-toggle').not(this).removeClass('active').next().slideUp(200)
+// AOS refresh
+$(window).on('load', function () {
+	AOS.refresh()
 })
+
+$(window).on('scroll', function () {
+	AOS.refresh()
+})
+// eof
+
+// Telephone mask
+if ($('*').is('input[type="tel"]')) {
+	$('input[type=tel]').inputmask({
+		mask: '+99 (999) 999 99 99',
+		showMaskOnHover: false,
+	})
+}
+// eof
+
+// Form submit
+$(
+	'.feedback__form, .footer__form, .popup-notice .popup__form, .popup-enroll .popup__form'
+).submit(function () {
+	var th = $(this)
+	$.ajax({
+		type: 'POST',
+		url: 'mail.php',
+		data: th.serialize(),
+	}).done(function () {
+		let a = document.createElement('a')
+
+		document.body.appendChild(a)
+
+		a.classList.add('mfp')
+		a.href = '.popup-success-default'
+
+		$('.mfp').magnificPopup({
+			removalDelay: 300,
+			mainClass: 'mfp-fade',
+			fixedContentPos: false,
+			callbacks: {
+				open: function () {
+					body.css('overflow-y', 'hidden')
+				},
+				close: function () {
+					body.css('overflow-y', 'auto')
+				},
+			},
+		})
+
+		a.click()
+		a.remove()
+
+		th.trigger('reset')
+		th.find('input').blur()
+	})
+
+	return false
+})
+
+$('.popup-callback .popup__form').submit(function () {
+	var th = $(this)
+	$.ajax({
+		type: 'POST',
+		url: 'mail.php',
+		data: th.serialize(),
+	}).done(function () {
+		let a = document.createElement('a')
+
+		document.body.appendChild(a)
+
+		a.classList.add('mfp')
+		a.href = '.popup-success-callback'
+
+		$('.mfp').magnificPopup({
+			removalDelay: 300,
+			mainClass: 'mfp-fade',
+			fixedContentPos: false,
+			callbacks: {
+				open: function () {
+					body.css('overflow-y', 'hidden')
+				},
+				close: function () {
+					body.css('overflow-y', 'auto')
+				},
+			},
+		})
+
+		a.click()
+		a.remove()
+
+		th.trigger('reset')
+		th.find('input').blur()
+	})
+
+	return false
+})
+
+$('.popup-quiz .popup__form').submit(function () {
+	var th = $(this)
+	$.ajax({
+		type: 'POST',
+		url: 'mail.php',
+		data: th.serialize(),
+	}).done(function () {
+		let a = document.createElement('a')
+
+		document.body.appendChild(a)
+
+		a.classList.add('mfp')
+		a.href = '.popup-success-quiz'
+
+		$('.mfp').magnificPopup({
+			removalDelay: 300,
+			mainClass: 'mfp-fade',
+			fixedContentPos: false,
+			callbacks: {
+				open: function () {
+					body.css('overflow-y', 'hidden')
+				},
+				close: function () {
+					body.css('overflow-y', 'auto')
+				},
+			},
+		})
+
+		a.click()
+		a.remove()
+
+		th.trigger('reset')
+		th.find('input').blur()
+	})
+
+	return false
+})
+// eof
+
+// Fixed header
+let didScroll
+let lastScrollTop = 0
+let delta = 5
+
+$(window).on('scroll', function (event) {
+	didScroll = true
+})
+
+if ($(window).width() >= 922) {
+	setInterval(function () {
+		if (didScroll) {
+			hasScrolled()
+			didScroll = false
+		}
+	}, 250)
+
+	function hasScrolled() {
+		let st = $(this).scrollTop()
+
+		if (Math.abs(lastScrollTop - st) <= delta) return
+
+		if (st > lastScrollTop && st > 0) {
+			// Scroll Down
+			header.removeClass('nav-down').addClass('nav-up').addClass('fixed')
+		} else {
+			// Scroll Up
+			if (st + $(window).height() < $(document).height()) {
+				header.removeClass('nav-up').addClass('nav-down')
+			}
+		}
+
+		if ($(window).scrollTop() <= 5) {
+			header.removeClass('fixed')
+		}
+
+		lastScrollTop = st
+	}
+} else {
+	$(window).on('scroll', function () {
+		if ($(this).scrollTop() > 5) {
+			header.addClass('fixed')
+		} else if ($(this).scrollTop() < 5) {
+			header.removeClass('fixed')
+		}
+	})
+}
+// eof
+
+// Hamburger
+toggleSidebar.on('click', function () {
+	hamburger.toggleClass('active')
+	sidebar.toggleClass('visible')
+	sidebarPlug.fadeToggle()
+	body.toggleClass('scroll_disabled')
+})
+// eof
+
+// Dropdown header
+headerDropdownBtn.on('mouseover', function () {
+	headerDropdownBtn.addClass('active')
+	headerDropdown.fadeIn(500).addClass('active')
+	headerDropdownPlug.fadeIn()
+	body.addClass('scroll_disabled')
+})
+
+header.on('mouseleave', function () {
+	headerDropdownBtn.removeClass('active')
+	headerDropdown.fadeOut(500).removeClass('active')
+	headerDropdownPlug.fadeOut()
+	body.removeClass('scroll_disabled')
+})
+// eof
+
+// Marquee
+if ($('*').is('.marquee')) {
+	$('.marquee').slick({
+		infinite: true,
+		arrows: false,
+		cssEase: 'linear',
+		speed: 8000,
+		autoplay: true,
+		autoplaySpeed: 0,
+		variableWidth: true,
+	})
+}
+// eof
+
+// Rec slider
+if ($('*').is('.recommends__slider')) {
+	$('.recommends__images').slick({
+		infinite: true,
+		arrows: false,
+		speed: 1000,
+		asNavFor: '.recommends__slider',
+		touchMove: false,
+		fade: true,
+	})
+
+	$('.recommends__slider').slick({
+		infinite: true,
+		arrows: false,
+		speed: 1000,
+		dots: true,
+		asNavFor: '.recommends__images',
+		touchMove: false,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		fade: true,
+	})
+}
 // eof
 
 // Input animation
@@ -34,368 +381,194 @@ $('input').each(function () {
 })
 // eof
 
-// Otp code
-if ($('*').is('.otp-form')) {
-	// pincode
-	let _pincode = []
-	_req = null
-
-	// main form
-	let $form = $('.otp-form')
-
-	// pincode group
-	let $group = $form.find('.otp-form__inputs')
-
-	// all input fields
-	let $inputs = $group.find(':input')
-
-	// input fields
-	let $first = $form.find('[name=pincode-1]'),
-		$second = $form.find('[name=pincode-2]'),
-		$third = $form.find('[name=pincode-3]'),
-		$fourth = $form.find('[name=pincode-4]'),
-		$fifth = $form.find('[name=pincode-5]'),
-		$sixth = $form.find('[name=pincode-6]')
-
-	// submit button
-	let $button = $form.find('.otp-form__submit')
-
-	// all fields
-	$inputs
-		.on('keyup', function (event) {
-			let code = event.keyCode || event.which
-
-			if (code === 9 && !event.shiftKey) {
-				// prevent default event
-				event.preventDefault()
-
-				// focus to submit button
-				$('.otp-form__submit').focus()
-			}
-		})
-		.inputmask({
-			mask: '9',
-			placeholder: '',
-			showMaskOnHover: false,
-			showMaskOnFocus: false,
-			clearIncomplete: true,
-			onincomplete: function () {
-				!debug || console.log('inputmask incomplete')
-			},
-			oncleared: function () {
-				let index = $inputs.index(this),
-					prev = index - 1,
-					next = index + 1
-
-				if (prev >= 0) {
-					// clear field
-					$inputs.eq(prev).val('')
-
-					// focus field
-					$inputs.eq(prev).focus()
-
-					// remove last nubmer
-					_pincode.splice(-1, 1)
-				} else {
-					return false
-				}
-
-				!debug || console.log('[oncleared]', prev, index, next)
-			},
-			onKeyValidation: function (key, result) {
-				let index = $inputs.index(this),
-					prev = index - 1,
-					next = index + 1
-
-				// focus to next field
-				if (prev < 6) {
-					$inputs.eq(next).focus()
-				}
-
-				!debug || console.log('[onKeyValidation]', index, key, result, _pincode)
-			},
-			onBeforePaste: function (data, opts) {
-				$.each(data.split(''), function (index, value) {
-					// set value
-					$inputs.eq(index).val(value)
-
-					!debug || console.log('[onBeforePaste:each]', index, value)
-				})
-
-				return false
-			},
-		})
-
-	// first field
-	$('[name=pincode-1]')
-		.on('focus', function (event) {
-			!debug || console.log('[1:focus]', _pincode)
-		})
-		.inputmask({
-			oncomplete: function () {
-				// add first character
-				_pincode.push($(this).val())
-
-				// focus to second field
-				$('[name=pincode-2]').focus()
-
-				!debug || console.log('[1:oncomplete]', _pincode)
-			},
-		})
-
-	// second field
-	$('[name=pincode-2]')
-		.on('focus', function (event) {
-			if (!($first.val().trim() !== '')) {
-				// prevent default
-				event.preventDefault()
-
-				// reset pincode
-				_pincode = []
-
-				// handle each field
-				$inputs.each(function () {
-					// clear each field
-					$(this).val('')
-				})
-
-				// focus to first field
-				$first.focus()
-			}
-
-			!debug || console.log('[2:focus]', _pincode)
-		})
-		.inputmask({
-			oncomplete: function () {
-				// add second character
-				_pincode.push($(this).val())
-
-				// focus to third field
-				$('[name=pincode-3]').focus()
-
-				!debug || console.log('[2:oncomplete]', _pincode)
-			},
-		})
-
-	// third field
-	$('[name=pincode-3]')
-		.on('focus', function (event) {
-			if (!($first.val().trim() !== '' && $second.val().trim() !== '')) {
-				// prevent default
-				event.preventDefault()
-
-				// reset pincode
-				_pincode = []
-
-				// handle each field
-				$inputs.each(function () {
-					// clear each field
-					$(this).val('')
-				})
-
-				// focus to first field
-				$first.focus()
-			}
-
-			!debug || console.log('[3:focus]', _pincode)
-		})
-		.inputmask({
-			oncomplete: function () {
-				// add third character
-				_pincode.push($(this).val())
-
-				// focus to fourth field
-				$('[name=pincode-4]').focus()
-
-				!debug || console.log('[3:oncomplete]', _pincode)
-			},
-		})
-
-	// fourth field
-	$('[name=pincode-4]')
-		.on('focus', function (event) {
-			if (
-				!(
-					$first.val().trim() !== '' &&
-					$second.val().trim() !== '' &&
-					$third.val().trim() !== ''
-				)
-			) {
-				// prevent default
-				event.preventDefault()
-
-				// reset pincode
-				_pincode = []
-
-				// handle each field
-				$inputs.each(function () {
-					// clear each field
-					$(this).val('')
-				})
-
-				// focus to first field
-				$first.focus()
-			}
-
-			!debug || console.log('[4:focus]', _pincode)
-		})
-		.inputmask({
-			oncomplete: function () {
-				// add fo fourth character
-				_pincode.push($(this).val())
-
-				// focus to fifth field
-				$('[name=pincode-5]').focus()
-
-				!debug || console.log('[4:oncomplete]', _pincode)
-			},
-		})
-
-	// fifth field
-	$('[name=pincode-5]')
-		.on('focus', function (event) {
-			if (
-				!(
-					$first.val().trim() !== '' &&
-					$second.val().trim() !== '' &&
-					$third.val().trim() !== '' &&
-					$fourth.val().trim() !== ''
-				)
-			) {
-				// prevent default
-				event.preventDefault()
-
-				// reset pincode
-				_pincode = []
-
-				// handle each field
-				$inputs.each(function () {
-					// clear each field
-					$(this).val('')
-				})
-
-				// focus to first field
-				$first.focus()
-			}
-
-			!debug || console.log('[5:focus]', _pincode)
-		})
-		.inputmask({
-			oncomplete: function () {
-				// add fifth character
-				_pincode.push($(this).val())
-
-				// focus to sixth field
-				$('[name=pincode-6]').focus()
-
-				!debug || console.log('[5:oncomplete]', _pincode)
-			},
-		})
-
-	// sixth field
-	$('[name=pincode-6]')
-		.on('focus', function (event) {
-			if (
-				!(
-					$first.val().trim() !== '' &&
-					$second.val().trim() !== '' &&
-					$third.val().trim() !== '' &&
-					$fourth.val().trim() !== '' &&
-					$fifth.val().trim() !== ''
-				)
-			) {
-				// prevent default
-				event.preventDefault()
-
-				// reset pincode
-				_pincode = []
-
-				// handle each field
-				$inputs.each(function () {
-					// clear each field
-					$(this).val('')
-				})
-
-				// focus to first field
-				$first.focus()
-			}
-
-			!debug || console.log('[6:focus]', _pincode)
-		})
-		.inputmask({
-			oncomplete: function () {
-				// add sixth character
-				_pincode.push($(this).val())
-
-				!debug || console.log('[6:oncomplete]', _pincode)
-			},
-		})
-}
-// eof
-
-// Remove product
-$('.product__remove').on('click', function () {
-	$(this).closest('.product').remove()
-})
-// eof
-
-// Select2
-if ($('*').is('.select2')) {
-	$('.select2').select2({
-		minimumResultsForSearch: -1,
-		width: '100%',
-		selectionCssClass: 'select__heading',
-		dropdownCssClass: 'select__dropdown',
+// Reviews slider
+if ($('*').is('.reviews__slider')) {
+	reviewsSlider.slick({
+		speed: 1000,
+		touchMove: false,
+		arrows: false,
 	})
-}
 
-if ($('*').is('.ship-select')) {
-	$('.ship-select').select2({
-		minimumResultsForSearch: -1,
-		width: '100%',
-		selectionCssClass: 'ship-select__heading select__heading',
-		dropdownCssClass: 'ship-select__dropdown select__dropdown',
+	$('.reviews__nav .reviews__slider-prev').on('click', function () {
+		reviewsSlider.slick('slickPrev')
+	})
+
+	$('.reviews__nav .reviews__slider-next').on('click', function () {
+		reviewsSlider.slick('slickNext')
+	})
+
+	reviewsSlider.on('afterChange', function (event, slick) {
+		$('.reviews__counter-current').html(slick.slickCurrentSlide() + 1)
+		$('.reviews__counter-all').html(slick.slideCount)
+	})
+
+	reviewsSliderMobile.slick({
+		speed: 500,
+		touchMove: false,
+		arrows: false,
+		adaptiveHeight: true,
+	})
+
+	$('.reviews__nav-mobile .reviews__slider-prev').on('click', function () {
+		reviewsSliderMobile.slick('slickPrev')
+	})
+
+	$('.reviews__nav-mobile .reviews__slider-next').on('click', function () {
+		reviewsSliderMobile.slick('slickNext')
+	})
+
+	reviewsSliderMobile.on('afterChange', function (event, slick) {
+		$('.reviews__counter-current').html(slick.slickCurrentSlide() + 1)
+		$('.reviews__counter-all').html(slick.slideCount)
+	})
+
+	$('.review').on('click', function () {
+		$(this).addClass('active')
+		$('.review')
+			.not(this)
+			.removeClass('active')
+			.removeClass('more-text')
+			.find('.review__text-more')
+			.removeClass('active')
+	})
+
+	$('.review__text-more').on('click', function () {
+		$(this).toggleClass('active').closest('.review').toggleClass('more-text')
 	})
 }
 // eof
 
-// Telephone mask
-if ($('*').is('input[type="tel"]')) {
-	$('input[type=tel]').inputmask({
-		mask: '+99 (999) 999 99 99',
-		showMaskOnHover: false,
-	})
+// Remove data-aos
+if ($(window).width() <= 992) {
+	$('.p-course .main__container, .posts__nav').removeAttr('data-aos')
+	$('.about__box, .post, .main__btns').removeAttr('data-aos-delay')
 }
 // eof
 
-// Gallery
-$('ul.gallery__captions').on('click', 'li:not(.active)', function () {
+// Tabs
+$('ul.portfolio__tabs-captions').on('click', 'li:not(.active)', function () {
 	$(this)
 		.addClass('active')
 		.siblings()
 		.removeClass('active')
-		.closest('.gallery')
-		.find('.gallery__content')
+		.closest('.portfolio__tabs')
+		.find('.portfolio__tabs-content')
 		.removeClass('active')
 		.eq($(this).index())
 		.addClass('active')
+
+	$('.portfolio__slider').slick('setPosition')
 })
 // eof
 
-// Active color
-$('.data__color-item').on('click', function () {
-	$(this).addClass('active').siblings().removeClass('active')
-})
+// Portfolio slider
+if ($('*').is('.portfolio__slider')) {
+	$('.portfolio__slider').slick({
+		slidesToShow: 3,
+		arrows: false,
+		speed: 500,
+		touchMove: false,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 1,
+					dots: true,
+				},
+			},
+		],
+	})
+}
 // eof
 
-// Active capacity
-$('.data__capacity-box').on('click', function () {
-	$(this).addClass('active').siblings().removeClass('active')
-})
+// Posts slider
+if ($('*').is('.posts__slider')) {
+	postsSlider.slick({
+		slidesToShow: 3,
+		arrows: false,
+		speed: 1000,
+		touchMove: false,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 1,
+					adaptiveHeight: true,
+					speed: 500,
+				},
+			},
+		],
+	})
+
+	$('.posts__nav .posts__slider-prev').on('click', function () {
+		postsSlider.slick('slickPrev')
+	})
+
+	$('.posts__nav .posts__slider-next').on('click', function () {
+		postsSlider.slick('slickNext')
+	})
+
+	postsSlider.on('afterChange', function (event, slick) {
+		$('.posts__counter-current').html(slick.slickCurrentSlide() + 1)
+		$('.posts__counter-all').html(slick.slideCount)
+	})
+}
 // eof
 
-// Product info dropdown
-$('.data__heading').on('click', function () {
-	$(this).toggleClass('active').next().slideToggle(200)
-})
+// CEO slider
+if ($('*').is('.ceo__slider')) {
+	$('.ceo__slider').slick({
+		arrows: false,
+		speed: 0,
+		touchMove: false,
+		dots: true,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					adaptiveHeight: true,
+				},
+			},
+		],
+	})
+
+	$('.ceo__slider').on('afterChange', function (event, slick) {
+		$('.ceo__slider')
+			.find('.slick-dots li.slick-active')
+			.prevAll()
+			.addClass('before')
+		$('.ceo__slider')
+			.find('.slick-dots li.slick-active')
+			.nextAll()
+			.removeClass('before')
+	})
+}
+// eof
+
+// Catalog marquee
+if ($('*').is('.catalog__slider')) {
+	$('.catalog__slider').slick({
+		infinite: true,
+		arrows: false,
+		cssEase: 'linear',
+		speed: 10000,
+		autoplay: true,
+		autoplaySpeed: 0,
+		variableWidth: true,
+	})
+}
+// eof
+
+// Gallery marquee
+if ($('*').is('.gallery__slider')) {
+	$('.gallery__slider').slick({
+		infinite: true,
+		arrows: false,
+		cssEase: 'linear',
+		speed: 15000,
+		autoplay: true,
+		autoplaySpeed: 0,
+		variableWidth: true,
+	})
+}
 // eof
