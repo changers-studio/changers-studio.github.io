8a309dcb37c37b4ce9de06880e61efd4509cb8a3
Nikita <nikita.tytskyi@gmail.com>
2022-12-12 00:40:42 +0400

    Isr

--- js/main.js
+++ js/main.js
@@ -1,10 +1,51 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
 	// Reviews slider
-	$('.reviews__slider').slick({
-		slidesToShow: 3,
-		speed: 800,
-		infinite: true,
-		rtl: true,
+	if ($('*').is('.reviews__slider')) {
+		$('.reviews__slider').slick({
+			slidesToShow: 3,
+			speed: 800,
+			infinite: true,
+			rtl: true,
+			touchMove: false,
+			responsive: [
+				{
+					breakpoint: 992,
+					settings: {
+						slidesToShow: 1,
+						arrows: false,
+					},
+				},
+			],
+		})
+	}
+	// ----------
+
+	// Gallery slider
+	if ($('*').is('.gallery__slider')) {
+		$('.gallery__slider').slick({
+			speed: 800,
+			infinite: true,
+			rtl: true,
+			touchMove: false,
+			dots: true,
+			responsive: [
+				{
+					breakpoint: 992,
+					settings: {
+						arrows: false,
+					},
+				},
+			],
+		})
+	}
+	// ----------
+})
+
+window.addEventListener('load', () => {
+	// AOS init
+	AOS.init({
+		duration: 1000,
+		once: true,
 	})
 	// ----------
 })
