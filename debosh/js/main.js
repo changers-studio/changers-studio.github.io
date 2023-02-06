ce38e4d085e7518e155d55dc554b38daae5ee356
Nikita <nikita.tytskyi@gmail.com>
2023-02-06 20:31:36 +0400

    com

--- debosh/js/main.js
+++ debosh/js/main.js
@@ -20,7 +20,7 @@ if ($(window).width() <= 922) {
 //
 
 // Input mask
-if ($("*").is('.input-phone')) {
+if ($('*').is('.input-phone')) {
 	$('.input-phone').inputmask({
 		mask: '(999) 99 99 999',
 		showMaskOnHover: false,
@@ -28,7 +28,6 @@ if ($("*").is('.input-phone')) {
 	})
 }
 //
-
 
 // Slider
 if ($('*').is('.portfolio__slider')) {
@@ -41,17 +40,155 @@ if ($('*').is('.portfolio__slider')) {
 			{
 				breakpoint: 992,
 				settings: {
-					centerMode: true
-				}
+					centerMode: true,
+				},
 			},
-		]
+		],
 	})
 }
+//
 
+// Fancybox
 $('[data-fancybox]').fancybox({
 	infobar: false,
-	buttons: [
-		"close"
-	],
+	buttons: ['close'],
 })
 //
+
+// Magnific-popup
+$('.mfp').magnificPopup({
+	removalDelay: 300,
+	mainClass: 'mfp-fade',
+	fixedContentPos: false,
+	callbacks: {
+		open: function () {
+			$('body').css('overflow-y', 'hidden')
+		},
+		close: function () {
+			$('body').css('overflow-y', 'auto')
+		},
+	},
+})
+// end
+
+// Range input
+if ($('*').is('.range')) {
+	let $range = $('.range__slider'),
+		$input = $('.range__data-input'),
+		instance,
+		min = 50,
+		max = 1000,
+		from = 0
+
+	$range.ionRangeSlider({
+		skin: 'round',
+		type: 'single',
+		min: min,
+		max: max,
+		from: 50,
+		to: 1000,
+		onStart: updateInputs,
+		onChange: updateInputs,
+	})
+
+	instance = $range.data('ionRangeSlider')
+
+	function updateInputs(data) {
+		from = data.from
+
+		$input.prop('value', from)
+	}
+
+	$input.on('input', function () {
+		var val = $(this).prop('value')
+
+		// validate
+		if (val < min) {
+			val = min
+		} else if (val > to) {
+			val = to
+		}
+
+		instance.update({
+			from: val,
+		})
+	})
+}
+//
+
+// Form elements
+$('.popup__boolean-btn, .popup__gift-item').on('click', function () {
+	$(this).addClass('active').siblings().removeClass('active')
+})
+//
+
+// Move links
+$('body').on('click', '.trigger-move', function (event) {
+	event.preventDefault()
+	var id = $(this).attr('href'),
+		top = $(id).offset().top
+	$('body,html').animate({ scrollTop: top }, 1500)
+})
+//
+
+// Form submit
+// $('.feedback__form').submit(function () {
+// 	var th = $(this)
+// 	$.ajax({
+// 		type: 'POST',
+// 		url: 'mail.php',
+// 		data: th.serialize(),
+// 	}).done(function () {
+// 		th.find('.feedback__form-success').fadeIn()
+
+// 		setTimeout(() => {
+// 			th.find('.feedback__form-success').fadeOut()
+// 		}, 3000)
+
+// 		th.trigger('reset')
+// 		th.find('input').blur()
+// 	})
+
+// 	return false
+// })
+
+// $(
+// 	'.feedback__form, .footer__form, .popup-notice .popup__form, .popup-enroll .popup__form, .product__disabled-form'
+// ).submit(function () {
+// 	var th = $(this)
+// 	$.ajax({
+// 		type: 'POST',
+// 		url: 'mail.php',
+// 		data: th.serialize(),
+// 	}).done(function () {
+// 		let a = document.createElement('a')
+
+// 		document.body.appendChild(a)
+
+// 		a.classList.add('mfp')
+// 		a.href = '.popup-success-default'
+
+// 		$('.mfp').magnificPopup({
+// 			removalDelay: 300,
+// 			mainClass: 'mfp-fade',
+// 			fixedContentPos: false,
+// 			callbacks: {
+// 				open: function () {
+// 					body.css('overflow-y', 'hidden')
+// 				},
+// 				close: function () {
+// 					body.css('overflow-y', 'auto')
+// 				},
+// 			},
+// 		})
+
+// 		a.click()
+// 		a.remove()
+
+// 		th.trigger('reset')
+// 		th.find('input').blur()
+// 	})
+
+// 	return false
+// })
+// ----------
