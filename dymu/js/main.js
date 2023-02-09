56a1ceb1e92811d4b6999833d0eefdfe765a82fd
Nikita <nikita.tytskyi@gmail.com>
2023-02-09 13:19:41 +0400

    Com

--- dymu/js/main.js
+++ dymu/js/main.js
@@ -100,30 +100,156 @@ $('.unique__btn-toggle').on('click', function () {
 //
 
 // Video
-// if ($('*').is('video')) {
-// 	const videoOptions = {
-// 		muted: true,
-// 		volume: 0,
-// 		autoplay: true,
-// 		loop: true,
-// 	}
+if ($('*').is('video')) {
+	const videoOptions = {
+		muted: true,
+		volume: 0,
+		autoplay: true,
+		loop: true,
+	}
 
-// 	const player1 = videojs('video1', videoOptions)
-// 	const player2 = videojs('video2', videoOptions)
-// 	const player3 = videojs('video3', videoOptions)
-// 	const player4 = videojs('video4', videoOptions)
-// 	const player5 = videojs('video5', videoOptions)
-// 	const player6 = videojs('video6', videoOptions)
-// 	const player7 = videojs('video7', videoOptions)
+	if ($('*').is('#video1')) {
+		const player1 = videojs('video1', videoOptions)
 
-// 	document.addEventListener('DOMContentLoaded', () => {
-// 		player1.play()
-// 		player2.play()
-// 		player3.play()
-// 		player4.play()
-// 		player5.play()
-// 		player6.play()
-// 		player7.play()
-// 	})
-// }
+		document.addEventListener('DOMContentLoaded', () => {
+			player1.play()
+		})
+	}
+
+	if ($('*').is('#video2')) {
+		const player2 = videojs('video2', videoOptions)
+
+		document.addEventListener('DOMContentLoaded', () => {
+			player2.play()
+		})
+	}
+
+	if ($('*').is('#video3')) {
+		const player3 = videojs('video3', videoOptions)
+
+		document.addEventListener('DOMContentLoaded', () => {
+			player3.play()
+		})
+	}
+
+	if ($('*').is('#video4')) {
+		const player4 = videojs('video4', videoOptions)
+
+		document.addEventListener('DOMContentLoaded', () => {
+			player4.play()
+		})
+	}
+
+	if ($('*').is('#video5')) {
+		const player5 = videojs('video5', videoOptions)
+
+		document.addEventListener('DOMContentLoaded', () => {
+			player5.play()
+		})
+	}
+
+	if ($('*').is('#video6')) {
+		const player6 = videojs('video6', videoOptions)
+
+		document.addEventListener('DOMContentLoaded', () => {
+			player6.play()
+		})
+	}
+
+	if ($('*').is('#video7')) {
+		const player7 = videojs('video7', videoOptions)
+
+		document.addEventListener('DOMContentLoaded', () => {
+			player7.play()
+		})
+	}
+
+	if ($('*').is('#portfolio1')) {
+		const portfolio1 = videojs('portfolio1', videoOptions)
+
+		document.addEventListener('DOMContentLoaded', () => {
+			portfolio1.play()
+		})
+	}
+
+	if ($('*').is('#portfolio2')) {
+		const portfolio2 = videojs('portfolio2', videoOptions)
+
+		document.addEventListener('DOMContentLoaded', () => {
+			portfolio2.play()
+		})
+	}
+
+	if ($('*').is('#mainVideo')) {
+		const mainVideo = videojs('mainVideo', videoOptions)
+
+		document.addEventListener('DOMContentLoaded', () => {
+			mainVideo.play()
+		})
+	}
+}
+//
+
+// Input mask
+if ($('*').is('.input-phone')) {
+	$('.input-phone').inputmask({
+		mask: '(999) 99 99 999',
+		showMaskOnHover: false,
+		placeholder: '',
+	})
+}
+//
+
+// Magnific-popup
+if ($('*').is('.mfp')) {
+	$('.mfp').magnificPopup({
+		removalDelay: 300,
+		mainClass: 'mfp-fade',
+		fixedContentPos: false,
+		callbacks: {
+			open: function () {
+				$('body').css('overflow-y', 'hidden')
+			},
+			close: function () {
+				$('body').css('overflow-y', 'auto')
+			},
+		},
+	})
+}
+
+$('.popup__close').on('click', function () {
+	$.magnificPopup.close()
+})
+//
+
+// Form submit
+$('.popup__form').submit(function () {
+	var th = $(this)
+	$.ajax({
+		type: 'POST',
+		url: 'mail.php',
+		data: th.serialize(),
+	}).done(function () {
+		th.find('.popup__form-success').fadeIn()
+
+		setTimeout(() => {
+			th.find('.popup__form-success').fadeOut()
+			$.magnificPopup.close()
+		}, 3000)
+
+		th.trigger('reset')
+		th.find('input').blur()
+	})
+
+	return false
+})
+//
+
+// Move links
+$('body').on('click', '.trigger-move', function (event) {
+	event.preventDefault()
+	var id = $(this).attr('href'),
+		top = $(id).offset().top
+	$('body,html').animate({ scrollTop: top }, 1500)
+})
 //
