bd2399b5b49db003f9ba1a1da2750c945f7e9e6c
Nikita <nikita.tytskyi@gmail.com>
2023-10-12 19:30:38 +0400

    -

--- uwh/js/main.js
+++
@@ -1,7 +0,0 @@
-const hamburgerBtn = document.querySelector('.header-hamburger__btn')
-const hamburgerMenu = document.querySelector('.header-hamburger__menu')
-
-hamburgerBtn.addEventListener('click', () => {
-	document.body.classList.toggle('scroll-disabled')
-	hamburgerMenu.classList.toggle('active')
-})
