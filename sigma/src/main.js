dff9398d1e02dd9c189220c23544bd6dc39465d6
Nikita <nikita.tytskyi@gmail.com>
2025-06-05 03:02:02 +0400

    -

--- sigma/src/main.js
+++
@@ -1,9 +0,0 @@
-import './index.html'
-import './sass/main.sass'
-
-export * from './modules/dynamic-adapt.js'
-
-export * from './modules/smooth-scroll.js'
-export * from './modules/animation.js'
-export * from './modules/header.js'
-export * from './modules/dropdown.js'
--- sigma/src/modules/check.js
+++
@@ -1,4 +0,0 @@
-const desktop = window.innerWidth > 960
-const mobile = window.innerWidth <= 960
-
-export { desktop, mobile }
